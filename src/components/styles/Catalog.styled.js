2f68ee838b168e5d9ceb629f7af31e168844080d
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-19 22:43:56 +0200

    Added general padding for catalog page

---
+++ src/components/styles/Catalog.styled.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { fontSize } from "../constants/Constants.js";
+
+const CatalogStyled = styled.div`
+  padding-top: ${fontSize.fs48};
+`;
+
+export default CatalogStyled;
