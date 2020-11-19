7c4c4e11f09ecc32db8c43ab94854fd7584e7d87
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-19 22:41:53 +0200

    Created styled component for panel with options on item list

---
+++ src/components/styles/ProductOverviewPanel.styled.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { fontSize, colors } from "../constants/Constants.js";
+
+const ProductOverviewPanelStyled = styled.div`
+  padding-top: ${fontSize.fs48};
+  h1 {
+    font-family: "Poppins";
+    font-size: ${fontSize.fs36};
+    line-height: 1.1;
+    text-transform: uppercase;
+  }
+  li:first-child {
+    padding-left: 0;
+  }
+  a {
+    cursor: pointer;
+  }
+  a:hover,
+  .active {
+    color: ${colors.primary};
+  }
+  .options {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .buttons {
+    display: flex;
+  }
+  .buttons button {
+    margin-left: 15px;
+  }
+`;
+
+export default ProductOverviewPanelStyled;
