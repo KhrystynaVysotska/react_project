e86be10b8d861a442936d99645724fb58574e900
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-19 22:42:24 +0200

    Created styled component for 'load more' button

---
+++ src/components/styles/LoadMoreButton.styled.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { fontSize } from "../constants/Constants.js";
+
+const LoadMoreButtonStyled = styled.div`
+  margin-top: ${fontSize.fs24};
+  width: 100%;
+  text-align: center;
+`;
+
+export default LoadMoreButtonStyled;
