7e286839f077ead83e57a552fe95908747023b1b
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-19 22:43:22 +0200

    Changed left and right paddings for container

--- src/components/styles/Container.js
+++ src/components/styles/Container.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { fontSize } from "../constants/Constants.js";
 
 const Container = styled.div`
-  padding: ${fontSize.fs36} ${fontSize.fs208};
+  padding: ${fontSize.fs36} 16%;
 `;
 
 export default Container;
