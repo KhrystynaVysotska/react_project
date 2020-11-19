5268a363d7919f6e3d653f301b8034893ac81170
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-19 22:44:20 +0200

    Created styled component for list of cards

---
+++ src/components/styles/Cards.styled.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { fontSize } from "../constants/Constants.js";
+
+const CardsStyled = styled.div`
+  padding-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export default CardsStyled;
