a130bfd1bfaea60de37824b80b4c5a964b8eb5a6
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-29 22:50:06 +0200

    Modified layout to route to item page which is modal window

--- src/components/pages/Layout.js
+++ src/components/pages/Layout.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Home from "../pages/Home.js";
 import Catalog from "../pages/Catalog.js";
 import Cart from "../pages/Cart.js";
-
+import Item from "../pages/Item.js";
 function Layout() {
+  let location = useLocation();
+  let background = location.state && location.state.background;
+
   return (
-    <Switch>
-      <Route path="/cart">
-        <Cart />
-      </Route>
-      <Route path="/catalog">
-        <Catalog />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <>
+      <Switch location={background || location}>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/catalog">
+          <Catalog />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      {background && (
+        <Route path="/item/:id">
+          <Item />
+        </Route>
+      )}
+    </>
   );
 }
 
