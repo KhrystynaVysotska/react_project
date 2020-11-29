830bd012a2bf22a3b499846723f0873f3ccbdccc
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-30 00:03:41 +0200

    Prevented default submission

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
-import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-import DirectionsIcon from "@material-ui/icons/Directions";
 
 function SearchField({ handleChange }) {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <Paper component="form">
+    <Paper component="form" onSubmit={submitHandler}>
       <IconButton>
         <SearchIcon />
       </IconButton>
