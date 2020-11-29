11ddeb71fe476716a12586773f4f321e9c79cee7
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-11-29 22:48:22 +0200

    Added some font sizes to constants fils

--- src/components/constants/Constants.js
+++ src/components/constants/Constants.js
@@ -6,8 +6,15 @@ export const slides = [
   { url: "https://technext.github.io/cozastore/images/slide-03.jpg" },
 ];
 
+export const itemPreview = [
+  { url: "https://technext.github.io/cozastore/images/product-detail-01.jpg" },
+  { url: "https://technext.github.io/cozastore/images/product-detail-02.jpg" },
+  { url: "https://technext.github.io/cozastore/images/product-detail-03.jpg" },
+];
+
 export const fontSize = {
   fs4: DEFAULT_FONT_SIZE + "px",
+  fs8: DEFAULT_FONT_SIZE * 2 + "px",
   fs12: DEFAULT_FONT_SIZE * 3 + "px",
   fs16: DEFAULT_FONT_SIZE * 4 + "px",
   fs20: DEFAULT_FONT_SIZE * 5 + "px",
@@ -15,7 +22,12 @@ export const fontSize = {
   fs28: DEFAULT_FONT_SIZE * 7 + "px",
   fs36: DEFAULT_FONT_SIZE * 9 + "px",
   fs48: DEFAULT_FONT_SIZE * 12 + "px",
+  fs52: DEFAULT_FONT_SIZE * 13 + "px",
+  fs56: DEFAULT_FONT_SIZE * 14 + "px",
   fs60: DEFAULT_FONT_SIZE * 15 + "px",
+  fs64: DEFAULT_FONT_SIZE * 16 + "px",
+  fs68: DEFAULT_FONT_SIZE * 17 + "px",
+  fs72: DEFAULT_FONT_SIZE * 18 + "px",
   fs84: DEFAULT_FONT_SIZE * 21 + "px",
   fs100: DEFAULT_FONT_SIZE * 25 + "px",
   fs160: DEFAULT_FONT_SIZE * 40 + "px",
