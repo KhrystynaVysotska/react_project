c4f979e36770873cde995da0c17f9843de505023
Khrystyna <khrystynavysotska@gmail.com>
2020-12-12 21:59:23 +0200

    Added another reducer for sweaters in cart

--- src/context/reducers/combinedReducer.js
+++ src/context/reducers/combinedReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { favorites } from "./favorites";
 import { sweaters } from "./sweaters";
+import { selected } from "./selected";
 
 export default combineReducers({
   favorites,
   sweaters,
+  selected,
 });
