ae99790811da3a8e15391ec871c86a8f1d8e1a72
Khrystyna <khrystynavysotska@gmail.com>
2020-12-13 02:15:14 +0200

    Added some styles for group button

---
+++ src/components/styles/CustomButtonGroup.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { fontSize } from "../constants/Constants";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+const ButtonGroupStyled = styled(ButtonGroup)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default ButtonGroupStyled;
