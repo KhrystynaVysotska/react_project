d71747053d60c970907839858c588d774c1be7a2
Khrystyna <khrystynavysotska@gmail.com>
2020-12-20 23:57:15 +0200

    Added styles for success page

---
+++ src/components/styles/ModalSuccess.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { colors, fontSize } from "../constants/Constants";
+
+const ModalSuccessStyled = styled.div`
+  position: absolute;
+  left: 30%;
+  right: 30%;
+  top: 25%;
+  bottom: 25%;
+  display: flex;
+  background: ${colors.white};
+  padding: ${fontSize.fs28} ${fontSize.fs72};
+  align-items: center;
+  justify-content: center;
+  p {
+    font-family: "Poppins";
+    font-weight: bold;
+    color: #333333;
+    font-size: 18px;
+    line-height: 1.5;
+    padding: 0 15px;
+  }
+`;
+
+export default ModalSuccessStyled;
---
+++ src/components/styles/Success.styled.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSuccess = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.65);
+  z-index: 1000;
+`;
+
+export default StyledSuccess;
