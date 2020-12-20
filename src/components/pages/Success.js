c77d069746ea7244a4c1415a4f622bb7233770e2
Khrystyna <khrystynavysotska@gmail.com>
2020-12-20 23:52:29 +0200

    Creates success page

---
+++ src/components/pages/Success.js
@@ -0,0 +1,24 @@
+import React from "react";
+import StyledSuccess from "../styles/Success.styled";
+import ModalSuccessStyled from "../styles/ModalSuccess.styled";
+import { useHistory } from "react-router-dom";
+import success_image from "../../images/success_icon.jpg";
+
+function Success() {
+  let history = useHistory();
+
+  let back = (e) => {
+    e.stopPropagation();
+    history.goBack();
+  };
+  return (
+    <StyledSuccess onClick={back}>
+      <ModalSuccessStyled onClick={(e) => e.stopPropagation()}>
+        <img alt="success" src={success_image} />
+        <p>Submitted successfully</p>
+      </ModalSuccessStyled>
+    </StyledSuccess>
+  );
+}
+
+export default Success;
