c769f48b19967fcb844962809a1ab020d3574310
Khrystyna <khrystynavysotska@gmail.com>
2020-12-22 01:32:47 +0200

    Changed some styled

--- src/components/styles/FormContent.styled.js
+++ src/components/styles/FormContent.styled.js
@@ -6,9 +6,8 @@ const StyledFormContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: ${fontSize.fs28} ${fontSize.fs72};
+  padding: ${fontSize.fs12} ${fontSize.fs72};
   .title {
-    font-family: "Poppins";
     font-size: ${fontSize.fs28};
     color: #282828;
   }
