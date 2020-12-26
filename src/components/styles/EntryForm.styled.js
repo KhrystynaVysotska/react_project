df1fc5af0e3cd3b37c5c2b986182cc4aecfbeea6
Khrystyna <khrystynavysotska@gmail.com>
2020-12-26 23:42:15 +0200

    Added styled for login and registration backgrounds

---
+++ src/components/styles/EntryForm.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { colors } from "../constants/Constants";
+
+const EntryFormStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${colors.lightprimary};
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default EntryFormStyled;
